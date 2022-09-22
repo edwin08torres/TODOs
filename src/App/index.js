ffb35e2c3163a7556ed89132f07e29d764f9f5f2
edwin08torres <at2899743@gmail.com>
2022-09-21 19:40:15 -0600

    ya

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,55 @@ import { AppUI } from "./appUI";
 import { TodoPriveder } from "../TodoContext";
 // import { useLocalStorage } from "../TodoContext/useLocalStorage"
 
+// function App() {
+//   const [state, setState] = React.useState('estado compartido');
+
+//   return (
+//     <React.Fragment>
+//       <TaskHeader>
+//         <TaskCounter></TaskCounter>
+//         <TaskSearch></TaskSearch>
+//       </TaskHeader>
+//       <TaskList>
+//         <TaskItem state={state} />
+//       </TaskList>
+//     </React.Fragment>
+//   );
+// }
+
+// function TaskHeader({ children }) {
+//   return (
+//     <header>
+//       {children}
+//     </header>
+//   )
+// }
+
+// function TaskList({ children }) {
+//   return (
+//   <section className="TaskList--container">
+//     {children}
+//   </section>
+//   )
+// }
+
+
+// function TaskCounter(){
+//   return (
+//     <p>Hola maricon soy un task counter</p>
+//   )
+// }
+
+// function TaskSearch() {
+//   return <p>Hola maricon soy un task search</p>;
+// }
+
+// function TaskItem({state}) {
+//   return <p>Hola maricon soy un task item :{state}</p>;
+// }
+
 
 function App() {
-
-  
-
   return (
     <TodoPriveder>
       {/* // Enviando propiedas al archivo AppUI */}
