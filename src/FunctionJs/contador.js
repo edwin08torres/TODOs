b56bd6140e88067deee61fc96f409701cf05ea94
edwin08torres <at2899743@gmail.com>
2022-09-23 21:20:47 -0600

    actualizado

--- src/FunctionJs/contador.js
+++ src/FunctionJs/contador.js
@@ -1,13 +1,11 @@
 import React from "react";
-import '../css/Contador.css'
-function Contador({totalTask, completedTask}) {
+import "../css/Contador.css";
+function Contador({ totalTask, completedTask }) {
   return (
     <h2 className="title">
-      {" "}
-      Usted ha completado <span>{completedTask}</span> de{" "}
-      <span>{totalTask}</span> tareas
+      Usted ha completado <span>{completedTask}</span> de <span>{totalTask}</span> tareas
     </h2>
   );
 }
 
-export {Contador};
+export { Contador };
